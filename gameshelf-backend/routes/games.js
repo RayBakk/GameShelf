9a8208cad51cb32a357d25a03272d5c451fdb040
Dhuygy1 <r1008288@student.thomasmore.be>
2025-03-26 16:05:23 +0100

    Fix: added basic route to games router

--- gameshelf-backend/routes/games.js
+++ gameshelf-backend/routes/games.js
@@ -2,3 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const Game = require('../models/Game');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('API is working! 🎮');
+});
+
+module.exports = router;
