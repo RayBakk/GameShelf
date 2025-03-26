5fa3e098c2311dbe255fbcf8fb75f3a59c3ab8e5
Dhuygy1 <r1008288@student.thomasmore.be>
2025-03-26 16:10:03 +0100

    deleting MONGO_URI lines

--- gameshelf-backend/index.js
+++ gameshelf-backend/index.js
@@ -4,19 +4,16 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const gamesRoutes = require('./routes/games');
 
-dotenv.config(); // Load .env variables
+dotenv.config(); 
 
-const app = express(); // 👈 THIS defines app
+const app = express(); 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/games', gamesRoutes);
 
-// Connect to MongoDB and start the server
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 }).then(() => {
   console.log('✅ Connected to MongoDB');
   app.listen(PORT, () => {
