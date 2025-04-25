5f19f032d74c7ed51b3a4abda59d755538e793b7
Dhuygy1 <r1008288@student.thomasmore.be>
2025-04-25 10:27:17 +0200

    paths fix

--- gameshelf-backend/index.js
+++ gameshelf-backend/index.js
@@ -13,12 +13,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../gameshelf-frontend/build')));
 
 app.use('/api/games', gamesRoutes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../gameshelf-frontend/build/index.html'));
 });
 
 mongoose.connect(process.env.MONGO_URI, {})
